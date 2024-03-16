d3edd4bcccdfbee2efa36b7b5e5a4df78b4e25fd
Wansanuk <157240989+Wansanuk@users.noreply.github.com>
2024-03-15 23:12:55 -0600

    Tech-Moms Final Project

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("Sacred Rose, Co.");
 
+document.getElementById('button').addEventListener('click',
+    function () {
+        document.querySelector('.bg-modal').style.display = 'flex';
+    });
+
+document.querySelector('.close').addEventListener('click',
+    function () {
+        document.querySelector('.bg-modal').style.display = 'none';
+    });
